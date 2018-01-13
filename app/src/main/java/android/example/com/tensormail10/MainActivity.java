3c2da89dfa653447a8d931360959178429400606
Aakash Alurkar <aakash.alurkar95@gmail.com>
2018-01-13 16:45:23 +0530

    Initial Commit

--- app/src/main/java/android/example/com/tensormail10/MainActivity.java
+++ app/src/main/java/android/example/com/tensormail10/MainActivity.java
@@ -1,7 +1,10 @@
 package android.example.com.tensormail10;
 
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
