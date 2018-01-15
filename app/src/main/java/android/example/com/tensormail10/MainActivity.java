839c6f62a9a225e74a203260f64d33d8c1798db8
shreeya-joshi <shreeyajoshi96@gmail.com>
2018-01-15 16:05:02 +0530

    Email tabs + 1st page button
    Signed-off-by: shreeya-joshi <shreeyajoshi96@gmail.com>

--- app/src/main/java/android/example/com/tensormail10/MainActivity.java
+++ app/src/main/java/android/example/com/tensormail10/MainActivity.java
@@ -1,16 +1,30 @@
 package android.example.com.tensormail10;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button emailButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailButton=(Button) findViewById(R.id.Emailbuttonid);
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailIntent=new Intent(MainActivity.this,emailActivity.class);
+                startActivity(emailIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/android/example/com/tensormail10/priorityTabFragment.java
@@ -0,0 +1,21 @@
+package android.example.com.tensormail10;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Shreeya on 15-01-2018.
+ */
+
+public class priorityTabFragment extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.prioritytab,container,false);
+    }
+}
